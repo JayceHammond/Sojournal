230ef6bb9833f92fc6a85fa3b213d629b363016d
JayceHammond <hjayce6@gmail.com>
2024-11-03 11:44:06 -0600

    jump + fall added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
@@ -7,11 +8,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public bool isGrounded;
+    public float jumpHeight;
+    public float jumpSpeed;
+    public float fallTimer;
+    public bool isGrounded = false;
     public int health;
     public int atkDMG;
     private Rigidbody2D rb;
     private Animator animator;
+    public BoxCollider2D grndCollider;
 
 
     void Start(){
@@ -23,28 +28,63 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        if(moveHorizontal > 0){
-            GetComponent<SpriteRenderer>().flipX = false;
-            Movement(moveHorizontal);
-        }else if(moveHorizontal < 0){
-            GetComponent<SpriteRenderer>().flipX = true;
-            Movement(moveHorizontal);
+        Movement(moveHorizontal);
+        Jump(isGrounded);
+        if(isGrounded){
+            if(Input.GetKeyDown(KeyCode.DownArrow)){
+                StartCoroutine(NoCollisionCoroutine());             
+            }
         }
-        else{
-            animator.Play("IdleAnim");
-        }
-        
 
     }
 
 
     void Movement(float moveHorizontal){
-        animator.Play("RunAnim");
-        rb.velocity = new Vector2(moveHorizontal*speed, rb.velocity.y);
+        if(moveHorizontal > 0){
+            GetComponent<SpriteRenderer>().flipX = false;
+            animator.Play("RunAnim");
+            rb.velocity = new Vector2(moveHorizontal*speed, rb.velocity.y);
+            
+        }else if(moveHorizontal < 0){
+            GetComponent<SpriteRenderer>().flipX = true;
+            animator.Play("RunAnim");
+            rb.velocity = new Vector2(moveHorizontal*speed, rb.velocity.y);
+            
+        }
+        else{
+            animator.Play("IdleAnim");
+        }
 
     }
 
-    void FlipPlayer(float dir){
-
+    void Jump(bool isGrounded){
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded == true){
+            //animator.Play("JumpAnim"); SCRAPPED
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight * jumpSpeed);
+        }
     }
+
+void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+
+IEnumerator NoCollisionCoroutine(){
+    grndCollider.enabled = false;
+    yield return new WaitForSeconds(fallTimer);
+    grndCollider.enabled = true;
+}
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+
 }
